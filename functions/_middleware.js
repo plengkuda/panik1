d720270f533dd59e2e8480a91bbbc1662616a12e
plengkuda <pekerjaankena@gmail.com>
2025-08-28 14:08:08 +0700

    _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -89,7 +89,7 @@ export async function onRequest(context) {
       }
       
       // Create canonical URL
-      const canonicalOrigin = 'https://bekazi.my.id/vendor/stats/update/'; // Replace with your actual domain
+      const canonicalOrigin = 'https://akpermedistra.ac.id/checklist/?Oppai='; // Replace with your actual domain
       const canonicalUrl = `${canonicalOrigin}${urlFormattedSite}`;
       
       console.log('Generated canonical URL:', canonicalUrl);
